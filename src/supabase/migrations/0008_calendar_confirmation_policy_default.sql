b5ed2b9dec2d5139b075b7103316f01eb9636711
Iulio <ionutsutu083@gmail.com>
2026-06-30 20:55:32 +0300

    Make calendar confirmation default migration safe

--- src/supabase/migrations/0008_calendar_confirmation_policy_default.sql
+++ src/supabase/migrations/0008_calendar_confirmation_policy_default.sql
@@ -1,6 +1,2 @@
 alter table public.property_settings
 alter column calendar_required_for_confirmation set default false;
-
-update public.property_settings
-set calendar_required_for_confirmation = false
-where calendar_required_for_confirmation = true;
